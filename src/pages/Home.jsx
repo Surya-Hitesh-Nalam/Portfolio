2d3177173e0d7fbcdfa137df7d5afc5c62940516
Surya-Hitesh-Nalam <suryanalam1234@gmail.com>
2025-06-10 21:07:13 +0530

    cahnged styles

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+export const Home = ()=>{
+    return <div className="min-h-screen ">
+
+        {/*Theme Toogle */}
+
+
+
+        {/* Background Effect */}
+
+
+        {/* Navbar */}
+
+
+        {/* Main Content */}
+
+
+
+        {/*Router */}
+    </div>
+}
