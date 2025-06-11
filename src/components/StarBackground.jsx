bb666c368edf3a1ab429e6f55d02170c571e185c
Surya-Hitesh-Nalam <suryanalam1234@gmail.com>
2025-06-11 17:33:06 +0530

    changed toogle theme

---
+++ src/components/StarBackground.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+// id ,size,x,y,opacity , animationDuration
+
+export const StarBackground = () =>{
+    const [stars,setStars] = useState([]);
+
+    const generateStars = ()=>{
+        const numberOfStars = Math.floor((window.innerWidth * window.innerHeight) / 10000);
+        const newStars = [];
+
+        for(let i=0;i<numberOfStars;i++){
+            newStars.push({
+                id:i,
+                size:Math.random()*3+1,
+                x:Math.random()*100,
+                y:Math.random()*100,
+                opacity:Math.random()*0.5+0.5
+            })
+        }
+    }
+    return <div></div>
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
+import { StarBackground } from "../components/StarBackground"
+import { ThemeToogle } from "../components/ThemeToogle"
+
 export const Home = ()=>{
-    return <div className="min-h-screen ">
-
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         {/*Theme Toogle */}
-
-
+        
+        <ThemeToogle/>
 
         {/* Background Effect */}
 
-
+        <StarBackground />
         {/* Navbar */}
 
 
